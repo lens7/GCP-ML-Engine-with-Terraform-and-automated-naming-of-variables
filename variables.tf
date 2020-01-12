937ef92667aa1c9c581461977ac9dfbda68c15f3
Siddharth <43311132+lens7@users.noreply.github.com>
2020-01-12 22:50:20 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "BUCKET_NAME1" {  
+ default = "<name of bucket>"
+}
+
+variable "project-name" {
+   default = "<name of your project>"
+}
+
+variable "JOB_NAME" {
+   default = "<job name>"
+}
+
+variable "MODEL_NAME" {
+   default = "<madel name>"
+}
+
+variable "REGION" {
+ default = "us-central1"
+}
+
+
+variable "JOB_NAME1" {
+default = "<prediction name>"
+}
+
+
+variable "VER" {
+ default = "<versionname>"
+}
+
